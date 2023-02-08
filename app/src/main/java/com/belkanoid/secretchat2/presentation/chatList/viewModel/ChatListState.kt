a7408a344f4ec606f2aafea7ac13ab8fb78c6e64
Sultan <squarells1@mail.ru>
2023-02-09 03:34:31 +0500

    added viewModel

---
+++ app/src/main/java/com/belkanoid/secretchat2/presentation/chatList/viewModel/ChatListState.kt
@@ -0,0 +1,10 @@
+package com.belkanoid.secretchat2.presentation.chatList.viewModel
+
+import com.belkanoid.secretchat2.domain.model.Message
+
+sealed class ChatListState {
+    class Success(val data: List<Message>): ChatListState()
+    class Error(val message: String): ChatListState()
+    object Loading: ChatListState()
+    object Empty: ChatListState()
+}
--- app/src/main/java/com/belkanoid/secretchat2/presentation/chatList/viewModel/ChatListViewModel.kt
+++ app/src/main/java/com/belkanoid/secretchat2/presentation/chatList/viewModel/ChatListViewModel.kt
@@ -1,9 +1,37 @@
 package com.belkanoid.secretchat2.presentation.chatList.viewModel
 
 import androidx.lifecycle.ViewModel
+import com.belkanoid.secretchat2.domain.repository.ChatRepository
+import com.belkanoid.secretchat2.domain.util.SharedPreferences
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
 
 class ChatListViewModel @Inject constructor(
-
+    private val repository: ChatRepository,
+    private val sharedPreferences: SharedPreferences,
 ): ViewModel() {
+
+    private val _chatListSate = MutableStateFlow(ChatListState.Empty)
+    val chatListState = _chatListSate.asStateFlow()
+
+    suspend fun getQueue() {
+
+    }
+
+    suspend fun sendMessage() {
+
+    }
+
+    suspend fun createUser(userName: String) {
+
+    }
+
+    suspend fun getMessage() {
+
+    }
+
+    suspend fun getUser() {
+
+    }
 }
